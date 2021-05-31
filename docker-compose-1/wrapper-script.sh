711db26ec38003f361e7bb0a01e007f4de911794
Shuplat Orest <shuplatorst@gmail.com>
2021-05-31 09:12:59 +0300

    commit

--- docker-compose-1/wrapper-script.sh
+++ docker-compose-1/wrapper-script.sh
@@ -1,5 +1,3 @@
-/etc/init.d/mysql start
-
 cd
 git clone https://github.com/restmind/spring-jpa-iot.git
 cd spring-jpa-iot/
@@ -7,7 +5,4 @@ mvn install
 mvn clean package
 timeout 30s mvn spring-boot:run
 
-export MYSQL_PWD=12345678
-/usr/bin/mysql -u root -e "CREATE DATABASE IF NOT EXISTS mydb;"
-/usr/bin/mysql -u root -e "FLUSH PRIVILEGES;"
 mvn spring-boot:run
